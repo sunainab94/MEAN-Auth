51a7c03c05b19fa0a8329c3ce1da9a743f27bbd1
pampas93 <abhijit.93@hotmail.com>
2018-02-22 00:15:47 -0500

    Modal view using Angular route and custom directives for Login

--- public/js/angularScript.js
+++ public/js/angularScript.js
@@ -13,5 +13,46 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
 
     $routeProvider.otherwise({ redirectTo: '/' });
 
+    //console.log($window.history);
     $locationProvider.html5Mode({ enabled: true, requireBase: false });
 }]);
+
+// app.directive('siteHeader', function () {
+//     return {
+//         restrict: 'E',
+//         template: '<button class="btn">{{back}}</button>',
+//         scope: {
+//             back: '@back'
+//             },
+//         link: function(scope, element, attrs) {
+//             $(element[0]).on('click', function() {
+//                 $('body').removeClass('modal-open');
+//                 $('.modal-backdrop').remove();
+//                 history.back();
+//                 scope.$apply();
+//             });
+           
+//         }
+//     };
+// });
+
+app.directive('back', function() {
+    return {
+        restrict: 'E',
+        replace: true,
+        template: '<div></div>',
+        link: function($scope, element, attrs) {
+            $scope.clickMe= function() {
+                
+                $('body').removeClass('modal-open');
+                $('.modal-backdrop').remove();
+                history.back();
+
+                //scope.$apply();
+            }
+        }
+    }
+});
+
+function Ctrl($scope) {
+}
