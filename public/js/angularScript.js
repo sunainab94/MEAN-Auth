c96185198d900a32f51a04b8583b8ba6f7fa036d
pampas93 <abhijit.93@hotmail.com>
2018-02-27 05:22:48 -0500

    Registration and Login forms UI. Todo: redirect after register/login

--- public/js/angularScript.js
+++ public/js/angularScript.js
@@ -1,23 +1,27 @@
-var app = angular.module('myApp', ['ngMaterial', 'ngMessages','ngRoute'])
+var app = angular.module('myApp', ['ngMaterial', 'ngMessages', 'ngRoute'])
 
-app.controller('Login', function($scope) {
-    
-  });
+app.controller('Login', function ($scope) {
 
-  app.controller('Register', function($scope) {
-    
-  });
+});
+
+app.controller('Register', function ($scope) {
+
+    $scope.submitForm = function () {
+        $scope.date = $scope.dob;
+    }
+});
+
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 
     $routeProvider.when('/register',
         {
             templateUrl: 'auth/register.html',
-            controller : 'Register'
+            controller: 'Register'
         });
     $routeProvider.when('/login',
         {
             templateUrl: 'auth/login.html',
-            controller : 'Login'
+            controller: 'Login'
         });
 
     $routeProvider.otherwise({ redirectTo: '/' });
@@ -26,14 +30,15 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
     $locationProvider.html5Mode({ enabled: true, requireBase: false });
 }]);
 
-app.directive('back', function() {
+
+app.directive('back', function () {             //Go back to original url when modal is cancelled
     return {
         restrict: 'E',
         replace: true,
         template: '<div></div>',
-        link: function($scope, element, attrs) {
-            $scope.clickMe= function() {
-                
+        link: function ($scope, element, attributes) {
+            $scope.clickMe = function () {
+
                 $('body').removeClass('modal-open');
                 $('.modal-backdrop').remove();
                 history.back();
@@ -44,10 +49,22 @@ app.directive('back', function() {
     }
 });
 
-// function Register($scope){
+app.directive('compareTo', function () {        //Password and confirm password
+    return {
+        require: "ngModel",
+        scope: {
+            compareTolValue: "=compareTo"
+        },
+        link: function (scope, element, attributes, ngModel) {
 
-// }
+            ngModel.$validators.compareTo = function (modelValue) {
 
-// function Login($scope){
-    
-// }
+                return modelValue == scope.compareTolValue;
+            };
+
+            scope.$watch("compareTolValue", function () {
+                ngModel.$validate();
+            });
+        }
+    }
+});
--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/temp', function(req, res, next) {
+router.get('/temp', function (req, res, next) {
   res.render('temp', { title: 'Express' });
 });
 
-router.post('/login', function(req, res, next) {  //Test
+router.post('/login', function (req, res, next) {  //Test
 
   console.log(req.body);
   res.render('index', { title: 'Express' });
 });
 
-router.post('/register', function(req, res, next) {  //Test
-  
-    console.log(req.body);
-    res.render('index', { title: 'Express' });
-  });
+router.post('/register', function (req, res, next) {  //Test
+
+  console.log(req.body);
+  res.render('temp', { title: 'Express' });
+});
 
 module.exports = router;
